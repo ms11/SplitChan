68c89288254fbae8d5cdcc72d18b8aab159ff8ba
ms11 <msanyi11@freemail.hu>
2012-08-26 00:37:48 +0200

    change make_install output file name

--- make_install.py
+++ make_install.py
@@ -1,5 +1,6 @@
-import zipfile,os
-build = zipfile.ZipFile("./build.zip","w")
+import zipfile,os,datetime
+now = datetime.datetime.now().strftime("%Y.%m.%d,%H-%M")
+build = zipfile.ZipFile("./splitchan-"+now+".zip","w")
 os.chdir("./splitchan/bin/Release/")
 build.write("splitchan.exe")
 os.chdir("../../../")
