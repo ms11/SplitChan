a694f386c8378759d7a8b9c76b439a94c1648a45
ms11 <msanyi11@freemail.hu>
2012-08-26 00:33:19 +0200

    fix make_install

--- make_install.py
+++ make_install.py
@@ -1,5 +1,7 @@
-import zipfile
-build = zipfile.ZipFile(".\\build.zip","w")
-build.write(".\\splitchan\\bin\\Release\\splitchan.exe")
-build.write(".\\start.sh")
+import zipfile,os
+build = zipfile.ZipFile("./build.zip","w")
+os.chdir("./splitchan/bin/Release/")
+build.write("splitchan.exe")
+os.chdir("../../../")
+build.write("./start.sh")
 build.close()
