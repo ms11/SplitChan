10e71ad3a7a65e9e19c3b4add74321f8184e7812
ms11 <msanyi11@freemail.hu>
2012-08-26 00:16:02 +0200

    Initial commit

---
+++ splitchan/splitchan.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace splitchan {
+    partial class splitchan {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tag_name = new System.Windows.Forms.TextBox();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.sound_drop = new System.Windows.Forms.Button();
+            this.img_drop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tag_name
+            // 
+            this.tag_name.Location = new System.Drawing.Point(12, 133);
+            this.tag_name.Name = "tag_name";
+            this.tag_name.Size = new System.Drawing.Size(100, 20);
+            this.tag_name.TabIndex = 3;
+            this.tag_name.Text = "Tag";
+            this.tag_name.Enter += new System.EventHandler(this.tag_name_Enter);
+            this.tag_name.Leave += new System.EventHandler(this.tag_name_Leave);
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(118, 131);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(100, 23);
+            this.save_btn.TabIndex = 4;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // sound_drop
+            // 
+            this.sound_drop.AllowDrop = true;
+            this.sound_drop.Location = new System.Drawing.Point(12, 25);
+            this.sound_drop.Name = "sound_drop";
+            this.sound_drop.Size = new System.Drawing.Size(100, 100);
+            this.sound_drop.TabIndex = 1;
+            this.sound_drop.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.sound_drop.UseVisualStyleBackColor = true;
+            this.sound_drop.Click += new System.EventHandler(this.sound_drop_Click);
+            this.sound_drop.DragDrop += new System.Windows.Forms.DragEventHandler(this.sound_drop_DragDrop);
+            this.sound_drop.DragOver += new System.Windows.Forms.DragEventHandler(this.sound_drop_DragOver);
+            // 
+            // img_drop
+            // 
+            this.img_drop.AllowDrop = true;
+            this.img_drop.Location = new System.Drawing.Point(118, 25);
+            this.img_drop.Name = "img_drop";
+            this.img_drop.Size = new System.Drawing.Size(100, 100);
+            this.img_drop.TabIndex = 2;
+            this.img_drop.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.img_drop.UseVisualStyleBackColor = true;
+            this.img_drop.Click += new System.EventHandler(this.img_drop_Click);
+            this.img_drop.DragDrop += new System.Windows.Forms.DragEventHandler(this.img_drop_DragDrop);
+            this.img_drop.DragOver += new System.Windows.Forms.DragEventHandler(this.img_drop_DragOver);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Drag & Drop works too";
+            this.label1.UseMnemonic = false;
+            // 
+            // splitchan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 162);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.img_drop);
+            this.Controls.Add(this.sound_drop);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.tag_name);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "splitchan";
+            this.Text = "SplitChan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tag_name;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Button sound_drop;
+        private System.Windows.Forms.Button img_drop;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ splitchan/splitchan.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace splitchan {
+    public partial class splitchan : Form {
+        public splitchan() {
+            InitializeComponent();
+            if (!IsRunningOnMono()) {
+                if (!File.Exists(".\\audio.png")) {
+                    File.Create(".\\test.ogg").Close();
+                    var audio = System.Drawing.Icon.ExtractAssociatedIcon(".\\test.ogg");
+                    sound_drop.Image = audio.ToBitmap();
+                    audio.Dispose();
+                    sound_drop.Image.Save(".\\audio.png");
+                    File.Delete(".\\test.ogg");
+                } else {
+                    sound_drop.Image = Bitmap.FromFile(".\\audio.png");
+                }
+                if (!File.Exists(".\\image.png")) {
+                    File.Create(".\\test.png").Close();
+                    var image = System.Drawing.Icon.ExtractAssociatedIcon(".\\test.png");
+                    img_drop.Image = image.ToBitmap();
+                    image.Dispose();
+                    img_drop.Image.Save(".\\image.png");
+                    File.Delete(".\\test.png");
+                } else {
+                    img_drop.Image = Bitmap.FromFile(".\\image.png");
+
+                }
+            } else {
+                sound_drop.Image = new Bitmap(100, 100);
+                var gp = Graphics.FromImage(sound_drop.Image);
+                gp.DrawString("Sound", Font, Brushes.Black, new PointF(10, 10));
+                gp.Flush();
+                gp.Dispose();
+                img_drop.Image = new Bitmap(100, 100);
+                gp = Graphics.FromImage(img_drop.Image);
+                gp.DrawString("Image", Font, Brushes.Black, new PointF(10, 10));
+                gp.Flush();
+                gp.Dispose();
+            }
+        }
+        const int maxSize = 3 * 1024 * 1024 - 200;
+        Brush[] colors = new Brush[] { null, Brushes.Yellow, Brushes.Green, Brushes.Black, Brushes.Red, Brushes.Black, Brushes.Cyan, Brushes.Orange, Brushes.White };
+        void makeSplitFile(string imagefile, string soundfile, string tag, string outfile) {
+            var raw = new FileStream(soundfile, FileMode.Open);
+            var basefile = Image.FromFile(imagefile);
+            Bitmap bmp = new Bitmap(basefile);
+            int id = 1;
+            //List<Color> colors = new List<Color>();
+            do {
+                MemoryStream ms = new MemoryStream();
+                //Color c = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+                var gr = Graphics.FromImage(bmp);
+                /*bmp.SetPixel(0, 0, colors[id]);
+                bmp.SetPixel(1, 0, colors[id]);
+                bmp.SetPixel(1, 1, colors[id]);
+                bmp.SetPixel(0, 1, colors[id]);*/
+                var w = (int)bmp.Width / 20;
+                var h = (int)bmp.Height / 20;
+                gr.FillRectangle(colors[id], new Rectangle(0, 0, w, h));
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                var name = Path.ChangeExtension(outfile, id.ToString("D3")) + Path.GetExtension(outfile);
+                FileStream fs = new FileStream(name, FileMode.Create);
+                ms.Position = 0;
+                do {
+                    byte[] buffer = new byte[1024];
+                    int l = ms.Read(buffer, 0, 1024);
+                    if (l < 1) break;
+                    fs.Write(buffer, 0, l);
+                } while (true);
+                //ms.CopyTo(fs);
+                uint start = (uint)fs.Position;
+                do {
+                    byte[] data2 = new byte[1024];
+                    int l = raw.Read(data2, 0, 1024);
+                    if (l < 1) break;
+                    fs.Write(data2, 0, l);
+                } while (fs.Length < (maxSize - 1024));
+                byte[] taga = Encoding.ASCII.GetBytes(tag + '.' + id.ToString("D3"));
+                ushort counter = (ushort)(taga.Length + 9);
+                uint end = (uint)fs.Position - 1;
+                fs.WriteByte((byte)taga.Length);
+                fs.Write(taga, 0, taga.Length);
+                fs.Write(BitConverter.GetBytes(start), 0, 4);
+                fs.Write(BitConverter.GetBytes(end), 0, 4);
+                byte[] cbytes = BitConverter.GetBytes(counter);
+                fs.Write(cbytes, 0, 2);
+                byte[] sign = Encoding.ASCII.GetBytes("4SPF");
+                fs.Write(sign, 0, 4);
+                fs.Close();
+                id++;
+            } while (raw.Position < raw.Length - 1);
+            raw.Close();
+        }
+        private void sound_drop_DragOver(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.Link;
+        }
+
+        private void img_drop_DragOver(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.Link;
+        }
+        private string ImgDrop;
+        private void img_drop_DragDrop(object sender, DragEventArgs e) {
+            var filename = ((string[])e.Data.GetData("FileNameW"))[0];
+            var ext = Path.GetExtension(filename);
+            if (ext != ".png" && ext != ".gif" && ext != ".jpg") {
+                MessageBox.Show("Not valid file!");
+            } else {
+                ImgDrop = filename;
+                img_drop.Text = Path.GetFileName(ImgDrop);
+            }
+
+        }
+        private string SoundDrop;
+        private void sound_drop_DragDrop(object sender, DragEventArgs e) {
+            var filename = ((string[])e.Data.GetData("FileNameW"))[0];
+            var ext = Path.GetExtension(filename);
+            if (ext != ".ogg") {
+                MessageBox.Show("Not valid file!");
+            } else {
+                SoundDrop = filename;
+                sound_drop.Text = Path.GetFileName(SoundDrop);
+            }
+        }
+
+        private void tag_name_Leave(object sender, EventArgs e) {
+            if (tag_name.Text == "")
+                tag_name.Text = "Tag";
+        }
+
+        private void tag_name_Enter(object sender, EventArgs e) {
+            tag_name.Text = "";
+        }
+
+        private void save_btn_Click(object sender, EventArgs e) {
+            var sfd = new SaveFileDialog();
+            var ext = Path.GetExtension(ImgDrop);
+            sfd.AddExtension = true;
+            sfd.Filter = "image file(*" + ext + ")|*" + ext;
+            sfd.FilterIndex = 0;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                makeSplitFile(ImgDrop, SoundDrop, tag_name.Text, sfd.FileName);
+            }
+        }
+        public static bool IsRunningOnMono() {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+        public void OpenFile(bool isimage) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = isimage ? "images (*.png,*.bmp,*.gif,*.jpg)|*.png;*.bmp;*.gif;*.jpg" : "sound(*.ogg)|*.ogg";
+            ofd.FilterIndex = 0;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                if (isimage) {
+                    ImgDrop = ofd.FileName;
+                    img_drop.Text = Path.GetFileName(ImgDrop);
+                } else {
+                    SoundDrop = ofd.FileName;
+                    sound_drop.Text = Path.GetFileName(SoundDrop);
+                }
+            }
+        }
+
+        private void sound_drop_Click(object sender, EventArgs e) {
+            OpenFile(false);
+        }
+
+        private void img_drop_Click(object sender, EventArgs e) {
+            OpenFile(true);
+        }
+    }
+}
